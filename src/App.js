91abd8b2ea3302043a0e52b6fc982a5525b56705
r0neel <ronbhagwakar@gmail.com>
2022-03-10 20:15:56 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header, Body, Footer } from "./components";
+import "./style.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
