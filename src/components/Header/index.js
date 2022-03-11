315c5a74079cd8e3e606a83bed899e8686900a3b
r0neel <ronbhagwakar@gmail.com>
2022-03-11 00:46:17 +0000

    Add: construction message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
+import Body from "../Body";
 
 const Header = () => {
+  const mainBody = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mainBody.current.scrollIntoView({ behaviour: "smooth" });
+  };
   return (
-    <header>
-      <h1>Hi I'm Ron</h1>
-    </header>
+    <>
+      <header>
+        <h1>Hi I'm Ron, I'm a full stack developer.</h1>
+        <button id="explore" onClick={handleSubmit}>
+          Explore
+        </button>
+      </header>
+      <section ref={mainBody}>
+        <Body />
+      </section>
+    </>
   );
 };
 export default Header;
